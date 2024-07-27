424e491fda562902804f05ae594bf6dd19249a32
emmanuel <emmanuelalba115@gmail.com>
2024-07-27 11:32:45 -0600

    refactor: :art: Refactored sql scripts in PersonaFisicaRepository
    Refactored sql scripts in PersonaFisicaRepository from using string interpolation to use parameters and avoid SQL injection

--- repository/PersonaFisicaRepository.cs
+++ repository/PersonaFisicaRepository.cs
@@ -18,52 +18,9 @@ namespace TEST_DEV_EPA_26072024.repository
             _context = context;
         }
 
-
-        public async Task<bool> AddPersonaFisica(AddPersonaFisicaDto personaFisicaToAdd)
-        {
-            string sql = @$"
-                EXECUTE dbo.sp_AgregarPersonaFisica 
-                @Nombre='{personaFisicaToAdd.Nombre}',
-                @ApellidoPaterno='{personaFisicaToAdd.ApellidoPaterno}',
-                @ApellidoMaterno='{personaFisicaToAdd.ApellidoMaterno}',
-                @RFC='{personaFisicaToAdd.RFC}',
-                @FechaNacimiento='{personaFisicaToAdd.FechaNacimiento}',
-                @UsuarioAgrega=0
-            ";
-
-            IDbConnection connection = _context.CreateConnection();
-            int rowsAffected = await connection.ExecuteAsync(sql);
-            return rowsAffected > 0;
-        }
-
-        public async Task<IEnumerable<PersonaFisica>> GetAllPersonasFisicas()
-        {
-            string query = @"
-                SELECT * FROM dbo.Tb_PersonasFisicas
-                WHERE Activo = 1;
-            ";
-
-            IDbConnection connection = _context.CreateConnection();
-            return await connection.QueryAsync<PersonaFisica>(query);
-        }
-
-        public async Task<PersonaFisica> GetPersonaFisicaById(int personaFisicaId)
-        {
-            string query = @$"
-                SELECT * FROM dbo.Tb_PersonasFisicas
-                WHERE IdPersonaFisica = {personaFisicaId} AND Activo = 1;
-            ";
-
-            IDbConnection connection = _context.CreateConnection();
-            PersonaFisica? personaFisica = await connection.QueryFirstOrDefaultAsync<PersonaFisica>(query);
-            return personaFisica;
-        }
-
-        public async Task<bool> DeletePersonaFisica(int personaFisicaId)
-        {
-            //  !   IMPORTANTE
-            //  EL StoredProcedure de eliminar del arhivo de scripts del archivo de las pruebas tiene 
-            //  la condicion invertida ya que debería especificar que si NO existe el usuario Activo 
+        //  !   IMPORTANTE
+            //  los StoredProcedures de eliminar y actualizar del archivo de scripts del archivo de las 
+            // pruebas tiene la condicion invertida ya que debería especificar que si NO existe el usuario Activo 
             //  marque el error:
 
             // IF EXISTS <---- Debería ser: IF NOT EXISTS
@@ -78,20 +35,74 @@ namespace TEST_DEV_EPA_26072024.repository
             //     THROW 50000, @ERROR, 1;
             // END;
 
-            //  Entonces haré mi propio sql script:
+            //  Entonces en esos dos Tasks hice mis propios sql scripts:
 
-            string sql = @$"
-                IF EXISTS (SELECT 1 FROM dbo.Tb_PersonasFisicas WHERE IdPersonaFisica = {personaFisicaId} AND Activo = 1)
-                    BEGIN
-                        UPDATE dbo.Tb_PersonasFisicas
-                        SET 
-                            Activo = 0
-                        WHERE IdPersonaFisica = {personaFisicaId};
-                    END
-                    ELSE
-                    BEGIN
-                        THROW 50000, 'El registro con IdPersonaFisica = {personaFisicaId} no existe o no está activo.', 1;
-                    END;
+        public async Task<bool> AddPersonaFisica(AddPersonaFisicaDto personaFisicaToAdd)
+        {
+            string sql = @"
+                EXECUTE dbo.sp_AgregarPersonaFisica 
+                    @Nombre,
+                    @ApellidoPaterno,
+                    @ApellidoMaterno,
+                    @RFC,
+                    @FechaNacimiento,
+                    @UsuarioAgrega
+            ";
+
+            var parameters = new
+            {
+                Nombre = personaFisicaToAdd.Nombre,
+                ApellidoPaterno = personaFisicaToAdd.ApellidoPaterno,
+                ApellidoMaterno = personaFisicaToAdd.ApellidoMaterno,
+                RFC = personaFisicaToAdd.RFC,
+                FechaNacimiento = personaFisicaToAdd.FechaNacimiento,
+                UsuarioAgrega = 0 
+            };
+
+            IDbConnection connection = _context.CreateConnection();
+            int rowsAffected = await connection.ExecuteAsync(sql, parameters);
+            return rowsAffected > 0;
+        }
+
+
+        public async Task<IEnumerable<PersonaFisica>> GetAllPersonasFisicas()
+        {
+            string query = @"
+                SELECT * FROM dbo.Tb_PersonasFisicas
+                WHERE Activo = 1;
+            ";
+
+            IDbConnection connection = _context.CreateConnection();
+            return await connection.QueryAsync<PersonaFisica>(query);
+        }
+
+        public async Task<PersonaFisica> GetPersonaFisicaById(int personaFisicaId)
+        {
+            string query = @"
+                SELECT * FROM dbo.Tb_PersonasFisicas
+                WHERE IdPersonaFisica = @IdPersonaFisica AND Activo = 1;
+            ";
+
+            IDbConnection connection = _context.CreateConnection();
+            PersonaFisica? personaFisica = await connection.QueryFirstOrDefaultAsync<PersonaFisica>(query, new { IdPersonaFisica = personaFisicaId });
+            return personaFisica;
+        }
+
+
+        public async Task<bool> DeletePersonaFisica(int personaFisicaId)
+        {
+            string sql = @"
+                IF EXISTS (SELECT 1 FROM dbo.Tb_PersonasFisicas WHERE IdPersonaFisica = @IdPersonaFisica AND Activo = 1)
+                BEGIN
+                    UPDATE dbo.Tb_PersonasFisicas
+                    SET 
+                        Activo = 0
+                    WHERE IdPersonaFisica = @IdPersonaFisica;
+                END
+                ELSE
+                BEGIN
+                    THROW 50000, 'El registro con IdPersonaFisica = @IdPersonaFisica no existe o no está activo.', 1;
+                END;
             ";
 
             using IDbConnection connection = _context.CreateConnection();
@@ -103,9 +114,9 @@ namespace TEST_DEV_EPA_26072024.repository
         public async Task<bool> UpdatePersonaFisica(int personaFisicaId, UpdatePersonaFisicaDto fieldsToUpdate)
         {
             string sqlSelect = @"
-        SELECT * FROM dbo.Tb_PersonasFisicas
-        WHERE IdPersonaFisica = @IdPersonaFisica AND Activo = 1; 
-    ";
+                SELECT * FROM dbo.Tb_PersonasFisicas
+                WHERE IdPersonaFisica = @IdPersonaFisica AND Activo = 1; 
+            ";
 
             IDbConnection connection = _context.CreateConnection();
 
